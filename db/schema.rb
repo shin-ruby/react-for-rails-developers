4fc9cdcee1c2c186689ff6f7abf9bd04271bee0a
shin-ruby <shin.php@gmail.com>
2020-02-26 13:29:57 +0800

    Make a basic User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_20_075909) do
+ActiveRecord::Schema.define(version: 2020_02_26_052127) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -50,6 +50,15 @@ ActiveRecord::Schema.define(version: 2020_02_20_075909) do
     t.boolean "completed"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "episodes", "sections"
